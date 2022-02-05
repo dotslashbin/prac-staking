fde1e1b36dc22bc873df796e2e195fcaa36ef444
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-02-05 19:29:22 +0800

    Initial implementation of functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,66 @@
 import React, { Component } from 'react'
-import Web3 from 'web3';
-
+import Web3 from 'web3'
+import TetherToken from '../build/Tether_Token.json'
+import StakingDapp from '../build/Staking_Dapp.json'
+import DummyToken from '../build/Dummy_Token.json'
 
 class App extends Component {
 
   async componentWillMount() {
     console.log('DEBUG: ... component mounted!')
     await this.loadWeb3()
-    // await this.loadBlockchainData()
+    await this.loadBlockchainData()
   }
 
-  // async loadBlockchainData(){
-  //   const web3 = window.web3
+  async loadBlockchainData(){
+    const web3 = window.web3
 
-  //   const accounts = await web3.eth.getAccounts()
-  //   this.setState({ account : accounts[0]})
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account : accounts[0]})
 
-  //   const netwrokId = await web3.eth.net.getId()
+    const netwrokId = await web3.eth.net.getId()
 
-  //   const TetherTokenData = TetherToken.networks[netwrokId]
+    const TetherTokenData = TetherToken.networks[netwrokId]
 
-  //   if(TetherTokenData){
+    if(TetherTokenData){
 
-  //     const tetherToken = new web3.eth.Contract(TetherToken.abi,TetherTokenData.address)
-  //     this.setState({tetherToken})
-  //     let tethertokenbalance = await tetherToken.methods.balance(this.state.account).call()
-  //     this.setState({ tethertokenbalance : tethertokenbalance.toString() })
+      const tetherToken = new web3.eth.Contract(TetherToken.abi,TetherTokenData.address)
+      this.setState({tetherToken})
+      let tethertokenbalance = await tetherToken.methods.balance(this.state.account).call()
+      this.setState({ tethertokenbalance : tethertokenbalance.toString() })
 
-  //   }else {
-  //     window.alert('Tether token contract not deployed to detected network.')
-  //   }
+    }else {
+      window.alert('Tether token contract not deployed to detected network.')
+    }
 
-  //   const DummyTokenData = DummyToken.networks[netwrokId]
+    const DummyTokenData = DummyToken.networks[netwrokId]
 
-  //   if(DummyTokenData){
+    if(DummyTokenData){
 
-  //     const dummyToken = new web3.eth.Contract(DummyToken.abi,DummyTokenData.address)
-  //     this.setState({dummyToken})
-  //     let dummytokenbalance = await dummyToken.methods.balance(this.state.account).call()
-  //     this.setState({dummytokenbalance : dummytokenbalance.toString()})
+      const dummyToken = new web3.eth.Contract(DummyToken.abi,DummyTokenData.address)
+      this.setState({dummyToken})
+      let dummytokenbalance = await dummyToken.methods.balance(this.state.account).call()
+      this.setState({dummytokenbalance : dummytokenbalance.toString()})
 
-  //   }else {
-  //     window.alert('Dummy token contract not deployed to detected network.')
-  //   }
+    }else {
+      window.alert('Dummy token contract not deployed to detected network.')
+    }
 
-  //   const StakingDappData = DummyToken.networks[netwrokId]
+    const StakingDappData = DummyToken.networks[netwrokId]
 
-  //   if(StakingDappData){
+    if(StakingDappData){
 
-  //     const stakingdapp = new web3.eth.Contract(StakingDapp.abi,StakingDappData.address)
-  //     this.setState({stakingdapp})
-  //     let stakingdappbalance = await stakingdapp.methods.stakingBalance(this.state.account).call()
-  //     this.setState({stakingdappbalance : stakingdappbalance.toString()})
-  //   }else {
-  //     window.alert('Staking Dapp contract not deployed to detected network.')
-  //   }
+      const stakingdapp = new web3.eth.Contract(StakingDapp.abi,StakingDappData.address)
+      this.setState({stakingdapp})
+      let stakingdappbalance = await stakingdapp.methods.stakingBalance(this.state.account).call()
+      this.setState({stakingdappbalance : stakingdappbalance.toString()})
+    }else {
+      window.alert('Staking Dapp contract not deployed to detected network.')
+    }
 
-  //   this.setState({ loading: false })
+    this.setState({ loading: false })
 
-  // }
+  }
 
   async loadWeb3() {
     if (window.ethereum) {
